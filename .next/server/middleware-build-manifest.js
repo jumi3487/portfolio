21ea5a59b80f8e4d85a6833872b8179a1bbefcbd
Jumi Pham <npham32@my.bcit.ca>
2025-03-17 11:29:42 -0700

    Five Layers Of Ocean Added

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -15,6 +15,11 @@ self.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/index.js"
     ],
+    "/Work": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/Work.js"
+    ],
     "/Work/FiveLayersOfOcean": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -129,7 +129,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("01df36f951d8c075")
+/******/ 		__webpack_require__.h = () => ("854b7df8dfe3bbd8")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
